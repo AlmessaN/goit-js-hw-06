7988261fd5449bc4c44437df8b716456f2bf9637
Stepan Almes <96116759+AlmessaN@users.noreply.github.com>
2022-09-19 12:45:41 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = document.querySelector('#ingredients');
+
+const liElements = ingredients.map((ingredients) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredients;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+
+listEl.append(...liElements);
